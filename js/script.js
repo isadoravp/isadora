9cadd1d10a8fca5d9d9a3a919c384fbae9aec6bf
isadoravp <petry_druid@hotmail.com>
2025-12-09 23:06:02 -0300

    script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// ÁUDIO PÁGINA 2
+/* ÁUDIO DA PÁGINA 2 */
 const audio = new Audio("assets/musica.mp3");
 audio.volume = 1.0;
 
-// ABRIR PÁGINA 2
+/* ABRIR PÁGINA 2 */
 const page1 = document.getElementById("page1");
 const page2 = document.getElementById("page2");
 
@@ -11,12 +11,27 @@ document.getElementById("openCard").addEventListener("click", () => {
   setTimeout(() => {
     page2.classList.add("active");
     audio.play().catch(()=>{});
-  }, 400);
-});
+    }, 400);
+  });
 
-// BOTÕES
-function openMaps() { window.open("https://www.google.com/maps?q=Vip+Festas+Porto+Alegre","_blank"); }
-function confirmarPresenca() { window.open("https://wa.me/5551992226662?text=Olá,%20confirmo%20minha%20presença%20no%20evento!","_blank"); }
-function openDress() { alert("Vestimenta: Esporte Fino"); }
-function openPresentes() { document.getElementById("modal-presentes").classList.add("active"); }
-function closePresentes() { document.getElementById("modal-presentes").classList.remove("active"); }
+/* BOTÕES */
+function openMaps(){
+  window.open("https://www.google.com/maps?q=Vip+Festas+Porto+Alegre", "_blank");
+}
+
+function confirmarPresenca() {
+  window.open("https://wa.me/5551992226662?text=Olá,%20confirmo%20minha%20presença%20no%20evento!","_blank");
+}
+
+function openDress() {
+  alert("Vestimenta: Esporte Fino");
+}
+
+function openPresentes() {
+  document.getElementById("modal-presentes").classList.add("active");
+}
+
+function closePresentes() {
+  document.getElementById("modal-presentes").classList.remove("active");
+}
+

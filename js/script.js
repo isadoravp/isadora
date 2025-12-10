8a70b37e3d053326599e47bd8a6e93d92e885655
isadoravp <petry_druid@hotmail.com>
2025-12-09 22:42:12 -0300

    script.js

--- js/script.js
+++ js/script.js
@@ -15,22 +15,8 @@ document.getElementById("openCard").addEventListener("click", () => {
 });
 
 // BOTÕES
-function openMaps(){
-  window.open("https://www.google.com/maps?q=Vip+Festas+Porto+Alegre", "_blank");
-}
-
-function confirmarPresenca() {
-  window.open("https://wa.me/5551992226662?text=Olá,%20confirmo%20minha%20presença%20no%20evento!", "_blank");
-}
-
-function openDress() {
-  alert("Vestimenta: Esporte Fino");
-}
-
-function openPresentes() {
-  document.getElementById("modal-presentes").classList.add("active");
-}
-
-function closePresentes() {
-  document.getElementById("modal-presentes").classList.remove("active");
-}
+function openMaps() { window.open("https://www.google.com/maps?q=Vip+Festas+Porto+Alegre","_blank"); }
+function confirmarPresenca() { window.open("https://wa.me/5551992226662?text=Olá,%20confirmo%20minha%20presença%20no%20evento!","_blank"); }
+function openDress() { alert("Vestimenta: Esporte Fino"); }
+function openPresentes() { document.getElementById("modal-presentes").classList.add("active"); }
+function closePresentes() { document.getElementById("modal-presentes").classList.remove("active"); }
